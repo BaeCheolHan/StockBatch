36174c7f043433ea4a0bb0146946438d8b82880b
BaeCheolHan <joker10421@gmail.com>
2023-07-12 15:57:20 +0900

    job 개발중

--- src/main/java/com/my/stock/job/NowStockPriceGettingJob.java
+++ src/main/java/com/my/stock/job/NowStockPriceGettingJob.java
@@ -2,17 +2,25 @@ package com.my.stock.job;
 
 import com.my.stock.base.BaseBatch;
 import com.my.stock.rdb.repository.StockRepository;
+import com.my.stock.util.ApiCaller;
+import com.my.stock.util.KisTokenProvider;
+import com.my.stock.util.RestKisToken;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.*;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -20,11 +28,21 @@ import java.util.List;
 @Configuration
 public class NowStockPriceGettingJob extends BaseBatch {
 
+	@Value("${api.kis.appKey}")
+	private String appKey;
+
+	@Value("${api.kis.app-secret}")
+	private String appSecret;
+
 	private final StockRepository stockRepository;
 
-	public NowStockPriceGettingJob(StockRepository stockRepository) {
+	private final KisTokenProvider kisTokenProvider;
+
+
+	public NowStockPriceGettingJob(StockRepository stockRepository, KisTokenProvider kisTokenProvider) {
 		super("NowStockPriceGettingJob", "0 0/20 * * * ?", null);
 		this.stockRepository = stockRepository;
+		this.kisTokenProvider = kisTokenProvider;
 	}
 
 	@Bean
@@ -61,7 +79,23 @@ public class NowStockPriceGettingJob extends BaseBatch {
 		}
 	};
 
-	private final ItemProcessor<String, String> nowStockPriceProcessor = item -> item + " test";
+	private final ItemProcessor<String, String> nowStockPriceProcessor = new ItemProcessor<>() {
+		@Override
+		public String process(String item) throws Exception {
+			RestKisToken kisToken = kisTokenProvider.getRestToken();
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("authorization", "Bearer " + kisToken.getAccess_token());
+			headers.add("appkey", appKey);
+			headers.add("appsecret", appSecret);
+			headers.add("tr_id", "FHKST01010100 ");
+
+			HashMap<String, Object> param = new HashMap<>();
+			param.put("FID_COND_MRKT_DIV_CODE", "J");
+			param.put("FID_INPUT_ISCD", item);
+			String response = ApiCaller.getInstance().get("https://openapi.koreainvestment.com:9443", headers, param);
+			return response;
+		}
+	};
 
 	private final ItemWriter<String> writer = item -> {
 		System.out.println(item);
---
+++ src/main/java/com/my/stock/util/SocketKisToken.java
@@ -0,0 +1,10 @@
+package com.my.stock.util;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SocketKisToken {
+	private String approval_key;
+}
